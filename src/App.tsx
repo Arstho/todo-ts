90ea35060eeddeb555b93061debdbf0a23af7e58
Aslan <asln86@mail.ru>
2023-07-17 05:15:48 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const [todo, setTodo] = React.useState([
+  {id: 1, title: 'vhkbjlnm', complited: false},
+  {id: 2, title: 'vhkbjlnm', complited: false},
+  {id: 3, title: 'vhkbjlnm', complited: false}
+])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
