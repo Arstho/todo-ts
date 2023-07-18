74131c35e5849ebe0fed664116b40ea748e83793
Aslan <asln86@mail.ru>
2023-07-18 07:47:00 +0300

    styles...compleded

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import './styles.css'
+import styles from './styles.module.scss';
 
-const TodoItem = ({ todo, handleClick }) => {
+
+const TodoItem = ({ todo, index, removeClick, handleChange }) => {
 
   return (
-    <div className='todo'>
-      <input type="checkbox" />
-      {todo.id}{' '}{todo.title}
-      <button onClick={() => handleClick(todo.id)}>x</button>
-    </div>
+    <li className={styles.li}>
+      <input type="checkbox" checked={todo.completed} onChange={() => handleChange(todo.id)}/>
+      <span className={todo.completed ? styles.done : undefined}>{index + 1}&nbsp;{todo.title}</span>
+      <button onClick={() => removeClick(todo.id)}>&times;</button>
+    </li>
   )
 }
 
-export default TodoItem
+export default React.memo(TodoItem)
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import TodoItem from './TodoItem'
+import styles from './styles.module.scss'
 
 // type TodoListProps = {
 //   todos: ITodo[]
@@ -9,22 +10,68 @@ import TodoItem from './TodoItem'
 
 const TodoList = () => {
   const [todos, setTodos] = React.useState([
-    { id: 1, title: 'vhkbjlnm', completed: false },
+    { id: 1, title: 'vhkbjlnm', completed: true },
     { id: 2, title: 'vhkbjlnm', completed: false },
     { id: 3, title: 'vhkbjlnm', completed: false }
   ])
+  const [txtTodo, setTxtTodo] = React.useState('')
+  const inputRef = React.useRef(null)
 
-  const removeClick = (id) => {
-    console.log('delete', id);
+  console.log(todos);
+
+  const removeClick = React.useCallback((id) => {
     setTodos(prev => prev.filter(todo => todo.id !== id))
+  }, [])
+
+  const changeHandler = (e) => {
+    e.preventDefault()
+    setTxtTodo(e.target.value)
+  }
+
+  const keyPressHandler = (e) => {
+    if (e.key === 'Enter') {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          title: txtTodo,
+          completed: false
+        }]
+      )
+    }
+  }
+
+  const handleChange = (id) => {
+    setTodos(todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    }))
   }
 
   return (
-    <div>
-      {todos.map(todo => {
-        return <TodoItem key={todo.id} todo={todo} handleClick={removeClick} />
-      })}
-    </div>
+    <>
+      <form action="">
+        <input
+          ref={inputRef}
+          type="text"
+          value={txtTodo}
+          onChange={changeHandler}
+          onKeyPress={keyPressHandler} />
+      </form>
+      <ul className={styles.ul}>
+        {todos?.map((todo, i) => {
+          return <TodoItem
+            key={todo.id}
+            todo={todo}
+            index={i}
+            removeClick={removeClick}
+            handleChange={handleChange}
+          />
+        })}
+      </ul>
+    </>
   )
 }
 
